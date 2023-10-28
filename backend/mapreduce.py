cb96ff389875ac652f9d3b191d532e20f2bf8609
Wyatt Guethlein <112825572+WGuethlein@users.noreply.github.com>
2023-10-28 19:27:09 -0400

    Implement pyspark word counter, based on asin

--- backend/mapreduce.py
+++ backend/mapreduce.py
@@ -1,48 +1,29 @@
 ## mapreduce based on asin number
 #Input: asin number
 #Output: list of top 20 words? word bubble created and jpg'd?
-import findspark
-findspark.init()
-from pyspark.sql import SparkSession;
 
-f = open('testfile.txt', 'w')
+import time
 
+import pyspark.sql.functions as f
 
 
-spark = SparkSession.builder.master("local").appName("reviews").getOrCreate()
-sc=spark.sparkContext
+#filter dataframe, keep only the reviews that equal the asin input
+def filter_df(df, asin):
+    return df.select('asin', 'summary','reviewtext').filter(df.asin == asin)
 
-# drop selected columns, hopefully to reduce memory usage
-columnsToDrop = ['unixReviewTime', 'verified', 'vote', 'reviewerName', 'reviewTime']
+# go through each review, completely reformat filtered dataframe to count each word
+def getWords(df):
+    return df.withColumn('word', f.explode(f.split(f.col('reviewText'), ' ')))\
+    .groupBy('word').count()\
+    .sort('count', ascending=False)
 
-# create dataframe for json
-# I didn't know you could use pointers in Python?!
-df = spark.read.json("data/test.json").drop(*columnsToDrop)
+def getResult(df, asin):
+    filtered_df = filter_df(df, asin)
 
-# get the first few lines (default 10) of the sample
-def getHead(num=10):
-    df.show(num)
+    start = time.perf_counter()
+    d = getWords(filtered_df)
+    end = time.perf_counter()
 
-# show only the columns you wish, and the amount (default 10)
-def getCol(col, amount=10):
-    df.select(col).show(amount)
+    print("Total Time: " + str(end-start))
+    return d
 
-# output the distinct asin values
-def getAsins():
-    df.select('asin').distinct().write.csv('test.csv')
-    print("Rows: " + str(df.count()))
-
-test = ['asin','summary','reviewText']
-
-#show reviews for a given asin
-#Props -> fields: columns to see in result
-#         asin: amazon item ID
-#         amount: amount of rows to show
-def getReviews(fields, asin="B017O9P72A", amount=20):
-    df.filter(df.asin == asin).select(*fields).show(20)
-
-
-#getHead(20)
-#getCol('asin')
-getAsins()
-#getReviews(test, "B017OBMC1M")
--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,43 @@
 # main backend file
-
 # websocket api? might work best
+
+#im not really sure what this does, but its needed
+import findspark
+findspark.init()
+
+#pyspark imports
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+
+# other function imports
+from mapreduce import getResult
+
+def main():
+    spark_conf = SparkConf()\
+    .setAppName("YourTest")\
+    .setMaster("local[*]")
+
+    sc = SparkContext.getOrCreate(spark_conf)
+
+    spark = SparkSession.builder\
+            .master("local")\
+            .appName("Colab")\
+            .config('spark.ui.port', '4050')\
+            .getOrCreate()
+
+
+    df = spark.read.json('data/amazon_sample.json')
+
+    asin = "B017O9P72A"
+    words = getResult(df, asin)
+    words.show(10)
+
+    #attempt second search while in same context
+    asin = "B017OBSCOS"
+    words = getResult(df, asin)
+    words.show(10)
+    
+
+
+if __name__ == "__main__":
+    main()
